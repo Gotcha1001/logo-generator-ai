6ca55fb82d38ee28ceb90bc5da27d089e2685183
WesleyWayneOlivier <WesleyOlivier443@gmail.com>
2025-01-20 21:14:18 +0200

    Live

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button } from '@/components/ui/button'
 import { UserButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
@@ -13,18 +13,17 @@ function Header() {
         <div className='px-10 lg:px-32 xl:px-48 2xl:px-56 p-4 flex justify-between items-center gradient-background2'>
             <Link href={'/'}>
                 <Image
-                    className='h-10 w-32 rounded-xl p-1 border-2 border-indigo-600 bg-black hover:scale-105 transition-all   '
+                    className='h-10 w-32 rounded-xl p-1 border-2 border-indigo-600 bg-black hover:scale-105 transition-all'
                     src="/logo3.jpg" alt='Logo' height={100} width={100} />
             </Link>
             <div className='flex gap-4 items-center'>
-
-                {user ?
-
-                    <Button variant="sex1">Dashboard</Button>
-
-                    :
+                {user ? (
+                    <Link href="/dashboard">
+                        <Button variant="sex1">Dashboard</Button>
+                    </Link>
+                ) : (
                     <Button>Get Started</Button>
-                }
+                )}
                 <UserButton
                     appearance={{
                         elements: {
@@ -33,11 +32,8 @@ function Header() {
                     }}
                 />
             </div>
-
-
-
         </div>
     )
 }
 
-export default Header
+export default Header;
